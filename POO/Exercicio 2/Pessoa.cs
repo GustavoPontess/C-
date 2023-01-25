9c5d485f98a2c78c48df9df68647ae9982d8acb8
GustavoPontess <gut.n.pontes@gmail.com>
2023-01-25 11:42:24 -0300

    POO 004 - Exercicio 2

---
+++ POO/Exercicio 2/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    public class Pessoa
+    {
+        // Definindo os tipos 
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ POO/Exercicio 2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarando variavel do tipo class Pessoa
+            Pessoa user1, user2;
+            string result = "";
+            Console.Clear();
+            // Instanciando a classe e criando um objeto user usando o comando new.
+            user1 = new Pessoa();
+            user2 = new Pessoa();
+            // Solicitando entrada de dados
+            Console.WriteLine("Qual o seu nome?");
+            // Acessando um atributo do objeto e atribuindo o nome da pessoa
+            user1.Nome = Console.ReadLine()!;
+            Console.WriteLine("Qual a sua idade?");
+            user1.Idade = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Qual o seu nome?");
+            // Acessando um atributo do objeto e atribuindo o nome da pessoa
+            user2.Nome = Console.ReadLine()!;
+            Console.WriteLine("Qual a sua idade?");
+            user2.Idade = int.Parse(Console.ReadLine()!);
+            if (user1.Idade > user2.Idade)
+            {
+                result = user1.Nome;
+            }else{
+                result = user2.Nome ;
+            }
+
+            Console.WriteLine($"Pessoa mais velha: {result}");
+        }
+    }
+}

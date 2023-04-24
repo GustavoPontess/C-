e9ac3b0b6726ebb2b6718c753431b0a1b5f7714d
GustavoPontess <gut.n.pontes@gmail.com>
2023-04-24 17:37:19 -0300

    Lista de Exercicios - Coleções

--- AEDS/Lista Colecoes/Lista de exercicios/Lista.cs
+++ AEDS/Lista Colecoes/Lista de exercicios/Lista.cs
@@ -9,18 +9,15 @@ namespace Lista_de_exercicios
     public class Lista
     {
         private ArrayList _elementos;
-        private int _n;
 
         public Lista()
         {
             _elementos = new ArrayList();
-            _n = 0;
         }
 
         public Lista(int num)
         {
             _elementos = new ArrayList(num);
-            _n = 0;
         }
 
         public void Inserir(int num)
@@ -29,17 +26,210 @@ namespace Lista_de_exercicios
         }
         public void Inserir(string num)
         {
-            _elementos.Add(
-                
-            );
+            _elementos.Add(num);
+        }
+        public void Inserir(ArrayList num)
+        {
+            _elementos.AddRange(num);
         }
 
+        public int SomaElementosFor()
+        {
+            try
+            {
+                int soma = 0;
+                for (int i = 0; i < _elementos.Count; i++)
+                {
+                    soma += (int)_elementos[i];
+                }
+                return soma;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return -1;
+            }
+        }
+        public int SomaElementosWhile()
+        {
+            int soma = 0, i = 0;
+            while (i < _elementos.Count)
+            {
+                soma += (int)_elementos[i];
+                i++;
+            }
+            return soma;
+        }
+        public int SomaElementosDoWhile()
+        {
+            int soma = 0, i = 0;
+            do
+            {
+                soma += (int)_elementos[i];
+                i++;
+            } while (i < _elementos.Count);
+            return soma;
+        }
+
+        public int SomaElementosForeach()
+        {
+            int soma = 0;
+            foreach (var item in _elementos)
+            {
+                soma += (int)item;
+            }
+            return soma;
+        }
+
+        public void RemoveAll(int num)
+        {
+            while (_elementos.Contains(num))
+            {
+                _elementos.Remove(num);
+            }
+        }
+
+        public void ContOcorrencia(int num)
+        {
+            ArrayList aux = new ArrayList();
+            int cont = 0;
+            aux = _elementos;
+            while (aux.Contains(num))
+            {
+                aux.Remove(num);
+                cont++;
+            }
+            Console.WriteLine($"Hã {cont} ocorrencias do numero {num} na coleção.");
+        }
+
+        public float MediaAritmetica(int soma)
+        {
+            return soma / _elementos.Count;
+        }
         public void Imprimir()
         {
             foreach (var num in _elementos)
             {
                 Console.Write($"{num}, ");
             }
+            Console.WriteLine();
+        }
+        public void ImprimirFor()
+        {
+            for (int i = 0; i < _elementos.Count; i++)
+            {
+                Console.Write($"{_elementos[i]}, ");
+            }
+            Console.WriteLine();
+        }
+        public void ImprimirReverso()
+        {
+            _elementos.Reverse();
+            for (int i = 0; i < _elementos.Count; i++)
+            {
+                Console.Write($"{_elementos[i]}, ");
+            }
+            _elementos.Reverse();
+            Console.WriteLine();
+        }
+        public void ImprimirImpares()
+        {
+            for (int i = 1; i < _elementos.Count; i += 2)
+            {
+                Console.Write($"{_elementos[i]}, ");
+            }
+            Console.WriteLine();
+        }
+        public void ImprimirElementosImpares()
+        {
+            for (int i = 0; i < _elementos.Count; i++)
+            {
+                if (((int)_elementos[i] % 2) != 0)
+                {
+                    Console.Write($"{_elementos[i]}, ");
+
+                }
+            }
+            Console.WriteLine();
+        }
+        public void ImprimirMetade()
+        {
+            for (int i = 0; i < 13; i++)
+            {
+                Console.Write($"{_elementos[i]}, ");
+            }
+            Console.WriteLine();
+        }
+
+        public void ImprimirRevForeach()
+        {
+            _elementos.Reverse();
+            foreach (var item in _elementos)
+            {
+                Console.Write($"{item}, ");
+            }
+            _elementos.Reverse();
+            Console.WriteLine();
+        }
+        public void ImprimirRevSemReverse()
+        {
+            // Cast(type) converte cada elemento do array do tipo objeto para o tipo especificado.
+            int[] Numeros = _elementos.Cast<int>().ToArray();
+
+            for (int i = _elementos.Count - 1; i >= 0; i--)
+            {
+                Console.Write($"{Numeros[i]}, ");
+            }
+            Console.WriteLine();
+        }
+        public void ImprimirImpaForeach()
+        {
+            int cont = 0;
+            foreach (var item in _elementos)
+            {
+                if (cont % 2 != 0)
+                {
+                    Console.Write($"{item}, ");
+                }
+                cont++;
+            }
+            Console.WriteLine();
+        }
+        public void ImprimirElementosForeach()
+        {
+            foreach (var item in _elementos)
+            {
+                if ((int)item % 2 != 0)
+                {
+                    Console.Write($"{item}, ");
+                }
+            }
+            Console.WriteLine();
+        }
+        public void ImprimirMetForeach()
+        {
+            int cont = 0;
+            foreach (var item in _elementos)
+            {
+                if (cont != 13)
+                {
+                    Console.Write($"{item}, ");
+                    cont++;
+                }
+            }
+            Console.WriteLine();
+        }
+        public int QuantosPositivos()
+        {
+            int soma = 0;
+            foreach (var item in _elementos)
+            {
+                if ((int)item > 0)
+                {
+                    soma ++;
+                }
+            }
+            return soma;
         }
     }
 }
--- AEDS/Lista Colecoes/Lista de exercicios/Program.cs
+++ AEDS/Lista Colecoes/Lista de exercicios/Program.cs
@@ -43,14 +43,6 @@ namespace Lista_de_exercicios
                         Console.Clear();
                         Questao8();
                         break;
-                    case 9:
-                        Console.Clear();
-                        Questao9();
-                        break;
-                    case 10:
-                        Console.Clear();
-                        Questao10();
-                        break;
                     case 11:
                         Console.Clear();
                         Questao11();
@@ -62,14 +54,6 @@ namespace Lista_de_exercicios
                     case 13:
                         Console.Clear();
                         Questao13();
-                        break;
-                    case 14:
-                        Console.Clear();
-                        Questao14();
-                        break;
-                    case 15:
-                        Console.Clear();
-                        Questao15();
                         break;
                     case 16:
                         Console.Clear();
@@ -111,18 +95,14 @@ namespace Lista_de_exercicios
 05  -- Questão 05
 06  -- Questão 06
 07  -- Questão 07
-08  -- Questão 08
-09  -- Questão 09
-10  -- Questão 10
+08  -- Questão 08, 09 e 10
 11  -- Questão 11
 12  -- Questão 12
-13  -- Questão 13
-14  -- Questão 14
-15  -- Questão 15
+13  -- Questão 13, 14 e 15
 16  -- Questão 16
 17  -- Questão 17
 18  -- Sair");
-            Console.Write("Digite o numero da questão: ");
+            Console.Write("Digite um numero do menu: ");
             try //colocando tratamento de exeção para caso o usuario digite algo que nao seja numeros
             {
                 op = int.Parse(Console.ReadLine()!);
@@ -208,6 +188,23 @@ namespace Lista_de_exercicios
             Fila Que = new Fila();
             Pilha Sta = new Pilha();
 
+            for (int i = 1; i < 100; i += 2)
+            {
+                List.Inserir(i);
+                Que.Inserir(i);
+                Sta.Inserir(i);
+            }
+            Console.Clear();
+            Console.WriteLine("===== ArrayList =====");
+            List.Imprimir();
+            Console.WriteLine(List.SomaElementosFor());
+            Console.WriteLine("\n=====   Queue   =====");
+            Que.Imprimir();
+            Console.WriteLine(Que.SomaElementos());
+            Console.WriteLine("\n=====   Stack   =====");
+            Sta.Imprimir();
+            Console.WriteLine(Sta.SomaElementos());
+
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
             Console.ReadKey();
@@ -216,54 +213,26 @@ namespace Lista_de_exercicios
         static void Questao4()
         {
             Console.WriteLine("====>> Questão 4");
-            ArrayList Elementos = new ArrayList(10);
-            Queue Q1 = new Queue(10); // Fila
-            Stack S1 = new Stack(10); // Pilha
+            Lista List = new Lista();
+            Fila Que = new Fila();
+            Pilha Sta = new Pilha();
 
-            Random R = new Random();
-            int numero = 0, Soma = 0, j = 0;
-
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i += 2)
             {
-                numero = R.Next(0, 100);
-                if (numero % 2 == 0)
-                {
-                    Elementos.Add(numero);
-                    Q1.Enqueue(numero); // Adiciona um elemento ao final do Queue 
-                    S1.Push(numero); //Adiciona um elemento ao topo da Pilha
-                }
-                else { i--; }
+                List.Inserir(i);
+                Que.Inserir(i);
+                Sta.Inserir(i);
             }
-
+            Console.Clear();
             Console.WriteLine("===== ArrayList =====");
-            foreach (var num in Elementos)
-            {
-                Console.Write($"{num}, ");
-            }
-            while (j < Elementos.Count)
-            {
-                Soma += (int)Elementos[j];
-                j++;
-            }
-            Console.WriteLine($" || Soma = {Soma}");
-            Soma = 0; j = 0;
-
+            List.Imprimir();
+            Console.WriteLine(List.SomaElementosWhile());
             Console.WriteLine("\n=====   Queue   =====");
-            foreach (var num in Q1)
-            {
-                Console.Write($"{num}, ");
-                Soma += (int)num;
-            }
-            Console.WriteLine($" || Soma = {Soma}");
-            Soma = 0;
-
+            Que.Imprimir();
+            Console.WriteLine(Que.SomaElementos());
             Console.WriteLine("\n=====   Stack   =====");
-            foreach (var num in S1)
-            {
-                Console.Write($"{num}, ");
-                Soma += (int)num;
-            }
-            Console.WriteLine($" || Soma = {Soma}");
+            Sta.Imprimir();
+            Console.WriteLine(Sta.SomaElementos());
 
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
@@ -273,53 +242,26 @@ namespace Lista_de_exercicios
         static void Questao5()
         {
             Console.WriteLine("====>> Questão 5");
-            ArrayList Elementos = new ArrayList(10);
-            Queue Q1 = new Queue(10); // Fila
-            Stack S1 = new Stack(10); // Pilha
+            Lista List = new Lista();
+            Fila Que = new Fila();
+            Pilha Sta = new Pilha();
 
-            Random R = new Random();
-            int Soma = 0, j = 0;
-
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i++)
             {
-                Elementos.Add(R.Next(0, 100));
-                Q1.Enqueue(R.Next(0, 100)); // Adiciona um elemento ao final da Queue
-                S1.Push(R.Next(0, 100)); // Adiciona um elemento ao topo da pilha
+                List.Inserir(i);
+                Que.Inserir(i);
+                Sta.Inserir(i);
             }
-
+            Console.Clear();
             Console.WriteLine("===== ArrayList =====");
-            foreach (var num in Elementos)
-            {
-                Console.Write($"{num}, ");
-            }
-
-            do
-            {
-                Soma += (int)Elementos[j];
-                j++;
-            }
-            while (j < Elementos.Count);
-
-
-            Console.WriteLine($" || Soma = {Soma}");
-            Soma = 0; j = 0;
-
+            List.Imprimir();
+            Console.WriteLine(List.SomaElementosDoWhile());
             Console.WriteLine("\n=====   Queue   =====");
-            foreach (var num in Q1)
-            {
-                Console.Write($"{num}, ");
-                Soma += (int)num;
-            }
-            Console.WriteLine($" || Soma = {Soma}");
-            Soma = 0;
-
+            Que.Imprimir();
+            Console.WriteLine(Que.SomaElementos());
             Console.WriteLine("\n=====   Stack   =====");
-            foreach (var num in S1)
-            {
-                Console.Write($"{num}, ");
-                Soma += (int)num;
-            }
-            Console.WriteLine($" || Soma = {Soma}");
+            Sta.Imprimir();
+            Console.WriteLine(Sta.SomaElementos());
 
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
@@ -329,50 +271,26 @@ namespace Lista_de_exercicios
         static void Questao6()
         {
             Console.WriteLine("====>> Questão 6");
-            ArrayList Elementos = new ArrayList(10);
-            Queue Q1 = new Queue(10); // Fila
-            Stack S1 = new Stack(10); // Pilha
+            Lista List = new Lista();
+            Fila Que = new Fila();
+            Pilha Sta = new Pilha();
 
-            Random R = new Random();
-            int numero = 0, Soma = 0;
-
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i += 2)
             {
-                numero = R.Next(0, 100);
-                if (numero % 2 == 0)
-                {
-                    Elementos.Add(numero);
-                    Q1.Enqueue(numero); // Adiciona um elemento ao final do Queue
-                    S1.Push(numero); // Adiciona um elemento ao topo da Pilha
-                }
-                else { i--; }
+                List.Inserir(i);
+                Que.Inserir(i);
+                Sta.Inserir(i);
             }
-
+            Console.Clear();
             Console.WriteLine("===== ArrayList =====");
-            foreach (var num in Elementos)
-            {
-                Console.Write($"{num}, ");
-                Soma += (int)num;
-            }
-            Console.WriteLine($" || Soma = {Soma}");
-            Soma = 0;
-
+            List.Imprimir();
+            Console.WriteLine(List.SomaElementosForeach());
             Console.WriteLine("\n=====   Queue   =====");
-            foreach (var num in Q1)
-            {
-                Console.Write($"{num}, ");
-                Soma += (int)num;
-            }
-            Console.WriteLine($" || Soma = {Soma}");
-            Soma = 0;
-
+            Que.Imprimir();
+            Console.WriteLine(Que.SomaElementos());
             Console.WriteLine("\n=====   Stack   =====");
-            foreach (var num in S1)
-            {
-                Console.Write($"{num}, ");
-                Soma += (int)num;
-            }
-            Console.WriteLine($" || Soma = {Soma}");
+            Sta.Imprimir();
+            Console.WriteLine(Sta.SomaElementos());
 
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
@@ -382,6 +300,32 @@ namespace Lista_de_exercicios
         static void Questao7()
         {
             Console.WriteLine("====>> Questão 7");
+            Lista List = new Lista();
+            Fila Que = new Fila();
+            Pilha Sta = new Pilha();
+
+            ArrayList entrada = new ArrayList() { 5, 13, 19, 31, 3, 7, 11, 5, 57, 13, 5 };
+
+            List.Inserir(entrada);
+            Que.Inserir(entrada);
+            Sta.Inserir(entrada);
+
+            Console.Clear();
+            Console.WriteLine("===== ArrayList =====");
+            List.Imprimir();
+            List.RemoveAll(5);
+            List.RemoveAll(13);
+            List.Imprimir();
+            Console.WriteLine("\n=====   Queue   =====");
+            Que.Imprimir();
+            Que.RemoveAll(5);
+            Que.RemoveAll(13);
+            Que.Imprimir();
+            Console.WriteLine("\n=====   Stack   =====");
+            Sta.Imprimir();
+            Sta.RemoveAll(5);
+            Sta.RemoveAll(13);
+            Sta.Imprimir();
 
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
@@ -390,30 +334,55 @@ namespace Lista_de_exercicios
         }
         static void Questao8()
         {
+            Console.WriteLine("====>> Questão 8, 9 e 10");
+            Lista List = new Lista();
+            Fila Que = new Fila();
+            Pilha Sta = new Pilha();
+
+            Console.WriteLine("Digite N numeros inteiros, Digite FIM para terminar.");
+            string? leitura = Console.ReadLine();
+            while (leitura != "FIM")
+            {
+                List.Inserir(int.Parse(leitura));
+                Que.Inserir(int.Parse(leitura));
+                Sta.Inserir(int.Parse(leitura));
+                leitura = Console.ReadLine();
+            }
+            Console.Clear();
+            List.Imprimir();
+            Console.WriteLine("===== ArrayList =====");
+            Console.WriteLine($"Soma: {List.SomaElementosFor()} | Media: {List.MediaAritmetica(List.SomaElementosFor())}");
+            Console.WriteLine($"Soma: {List.SomaElementosForeach()} | Media: {List.MediaAritmetica(List.SomaElementosForeach())}");
+            Console.WriteLine("\n=====   Queue   =====");
+            Console.WriteLine($"Soma: {Que.SomaElementos()} | Media: {Que.MediaAritmetica(Que.SomaElementos())}");
+            Console.WriteLine("\n=====   Stack   =====");
+            Console.WriteLine($"Soma: {Sta.SomaElementos()} | Media: {Sta.MediaAritmetica(Que.SomaElementos())}");
 
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
             Console.ReadKey();
             Console.Clear();
         }
-        static void Questao9()
-        {
 
-            Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
-            // basic use of "Console.ReadKey()" method
-            Console.ReadKey();
-            Console.Clear();
-        }
-        static void Questao10()
-        {
-
-            Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
-            // basic use of "Console.ReadKey()" method
-            Console.ReadKey();
-            Console.Clear();
-        }
         static void Questao11()
         {
+            Console.WriteLine("====>> Questão 11");
+            Lista List = new Lista();
+            Fila Que = new Fila();
+            Pilha Sta = new Pilha();
+
+            ArrayList entrada = new ArrayList() { 1, 2, "AED", new Queue(), "teste", 3.14 };
+
+            List.Inserir(entrada);
+            Que.Inserir(entrada);
+            Sta.Inserir(entrada);
+
+            Console.WriteLine("===== ArrayList =====");
+            Console.WriteLine($"Soma: {List.SomaElementosFor()}");
+            Console.WriteLine("\n=====   Queue   =====");
+            Console.WriteLine($"Soma: {Que.SomaElementos()}");
+            Console.WriteLine("\n=====   Stack   =====");
+            Console.WriteLine($"Soma: {Sta.SomaElementos()}");
 
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
@@ -422,6 +391,43 @@ namespace Lista_de_exercicios
         }
         static void Questao12()
         {
+            Console.WriteLine("====>> Questão 12");
+            Lista List = new Lista();
+            Fila Que = new Fila();
+            Pilha Sta = new Pilha();
+
+            for (int i = 2; i < 25; i++)
+            {
+                List.Inserir(i);
+                Que.Inserir(i);
+                Sta.Inserir(i);
+            }
+
+            Console.WriteLine("===== ArrayList =====");
+            Console.WriteLine("\n===== Usando For =====");
+            List.ImprimirFor();
+            List.ImprimirReverso();
+            List.ImprimirImpares();
+            List.ImprimirElementosImpares();
+            List.ImprimirMetade();
+            Console.WriteLine("\n===== Usando Foreach =====");
+            List.Imprimir();
+            List.ImprimirRevForeach();
+            List.ImprimirImpaForeach();
+            List.ImprimirElementosForeach();
+            List.ImprimirMetForeach();
+            Console.WriteLine("\n=====   Queue   =====");
+            Que.Imprimir();
+            Que.ImprimirRevForeach();
+            Que.ImprimirImpaForeach();
+            Que.ImprimirElementosForeach();
+            Que.ImprimirMetForeach();
+            Console.WriteLine("\n=====   Stack   =====");
+            Sta.Imprimir();
+            Sta.ImprimirRevForeach();
+            Sta.ImprimirImpaForeach();
+            Sta.ImprimirElementosForeach();
+            Sta.ImprimirMetForeach();
 
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
@@ -430,22 +436,35 @@ namespace Lista_de_exercicios
         }
         static void Questao13()
         {
+            Console.WriteLine("====>> Questão 13, 14 e 15");
+            Lista List = new Lista();
+            Fila Que = new Fila();
+            Pilha Sta = new Pilha();
 
-            Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
-            // basic use of "Console.ReadKey()" method
-            Console.ReadKey();
-            Console.Clear();
-        }
-        static void Questao14()
-        {
+            int n = 0;
+            Random r = new Random();
+            Console.Write("Informe a quantidade de numeros que deseja gerar: ");
+            n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
-            // basic use of "Console.ReadKey()" method
-            Console.ReadKey();
-            Console.Clear();
-        }
-        static void Questao15()
-        {
+            for (int i = 0; i < n; i++)
+            {
+                List.Inserir(r.Next(0, 100));
+                Que.Inserir(r.Next(0, 100));
+                Sta.Inserir(r.Next(0, 100));
+            }
+
+            Console.WriteLine("===== ArrayList =====");
+            List.Imprimir();
+            List.ImprimirRevSemReverse();
+            Console.WriteLine($"Soma: {List.SomaElementosForeach()}");
+            Console.WriteLine("\n=====   Queue   =====");
+            Que.Imprimir();
+            Que.ImprimirRevSemReverse();
+            Console.WriteLine($"Soma: {Que.SomaElementos()}");
+            Console.WriteLine("\n=====   Stack   =====");
+            Sta.Imprimir();
+            Sta.ImprimirRevSemReverse();
+            Console.WriteLine($"Soma: {Sta.SomaElementos()}");
 
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
@@ -454,6 +473,29 @@ namespace Lista_de_exercicios
         }
         static void Questao16()
         {
+            Console.WriteLine("====>> Questão 16");
+            Lista List = new Lista();
+            Fila Que = new Fila();
+            Pilha Sta = new Pilha();
+
+            ArrayList entrada = new ArrayList() { -5, 13, -19, 31, -3, -7, -11, 5, -57, 13, 5 };
+
+            List.Inserir(entrada);
+            Que.Inserir(entrada);
+            Sta.Inserir(entrada);
+            
+
+            Console.WriteLine("===== ArrayList =====");
+            Console.WriteLine($"Soma: {List.QuantosPositivos()}");
+            Console.WriteLine("\n=====   Queue   =====");
+            Console.WriteLine($"Soma: {Que.QuantosPositivos()}");
+            Console.WriteLine("\n=====   Stack   =====");
+            Console.WriteLine($"Soma: {Sta.QuantosPositivos()}");
+
+            Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
+            // basic use of "Console.ReadKey()" method
+            Console.ReadKey();
+            Console.Clear();
 
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
@@ -462,13 +504,33 @@ namespace Lista_de_exercicios
         }
         static void Questao17()
         {
+             Console.WriteLine("====>> Questão 17");
+            Lista List = new Lista();
+            Fila Que = new Fila();
+            Pilha Sta = new Pilha();
 
+            ArrayList entrada = new ArrayList() { 5, 13, 19, 31, 3, 7, 11, 5, 57, 13, 5 };
+
+            List.Inserir(entrada);
+            Que.Inserir(entrada);
+            Sta.Inserir(entrada);
+
+            Console.Clear();
+            Console.WriteLine("===== ArrayList =====");
+            List.ContOcorrencia(5);
+            List.ContOcorrencia(13);
+            Console.WriteLine("\n=====   Queue   =====");
+            Que.ContOcorrencia(5);
+            Que.ContOcorrencia(13);
+            Console.WriteLine("\n=====   Stack   =====");
+            Sta.ContOcorrencia(5);
+            Sta.ContOcorrencia(13);
             Console.WriteLine("\npressione qualquer tecla para voltar ao menu...");
             // basic use of "Console.ReadKey()" method
             Console.ReadKey();
             Console.Clear();
         }
 
-        
+
     }
 }
